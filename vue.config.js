297bed9cfc5e57f883140464bb72b9439cc72576
wsn <123@qq.com>
2023-05-31 09:36:53 +0800

    登录模块

--- vue.config.js
+++ vue.config.js
@@ -13,7 +13,7 @@ const name = defaultSettings.title || 'vue Admin Template' // page title
 // For example, Mac: sudo npm run
 // You can change the port by the following methods:
 // port = 9528 npm run dev OR npm run dev --port = 9528
-const port = process.env.VUE_APP_PORT
+const port = process.env.VUE_APP_PORT || 9528
 
 // All configuration item explanations can be find in https://cli.vuejs.org/config/
 module.exports = {
@@ -31,10 +31,27 @@ module.exports = {
   productionSourceMap: false,
   devServer: {
     port: port,
-    open: true,
+    open: false,
     overlay: {
       warnings: false,
       errors: true
+    },
+    // 代理跨域的配置
+    proxy: {
+      // 当我们的本地的请求 有/api的时候，就会代理我们的请求地址向另外一个服务器发出请求
+      // '/api': {
+      //   // target: 'http://ihrm-java.itheima.net/', // 跨域请求的地址
+      //   target: 'http://ihrm-java.itheima.net/', // 跨域请求的地址
+      //   // target: 'http://ihrm.itheima.net/', // 跨域请求的地址
+      //   changeOrigin: true // 只有这个值为true的情况下 才表示开启跨域
+      // }
+      '/api': {
+        target: 'http://localhost:9091',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': ''
+        }
+      }
     }
   },
   configureWebpack: {
