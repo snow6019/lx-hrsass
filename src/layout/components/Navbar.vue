5d3549afb7c772832baf510d634a2b9d33513c3d
wsn <123@qq.com>
2023-06-12 14:32:32 +0800

    update2

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -12,7 +12,8 @@
     <div class="right-menu">
       <el-dropdown class="avatar-container" trigger="click">
         <div class="avatar-wrapper">
-          <img :src="staffPhoto" class="user-avatar">
+          <!-- <img :src="staffPhoto" class="user-avatar"> -->
+          <img v-imagerror="defaultImg" :src="staffPhoto" class="user-avatar">
           <span class="name">{{ name }}</span>
           <i class="el-icon-caret-bottom" style="color:#fff" />
         </div>
@@ -46,6 +47,11 @@ export default {
   components: {
     // Breadcrumb,
     Hamburger
+  },
+  data() {
+    return {
+      defaultImg: require('@/assets/common/head.jpg')
+    }
   },
   computed: {
     ...mapGetters([
