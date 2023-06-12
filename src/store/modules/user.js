53d057844d4c8c887502dd806dabd8de6daea03c
wsn <123@qq.com>
2023-06-12 09:01:04 +0800

    update

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,4 +1,4 @@
-import { login, getUserInfo } from '@/api/user'
+import { login, getUserInfo, getUserDetailById } from '@/api/user'
 import { getToken, setToken } from '@/utils/auth'
 const state = {
   token: getToken() || '',
@@ -15,25 +15,37 @@ const mutations = {
 }
 const getters = {}
 const actions = {
-  login(context, data) {
-    return new Promise((resolve, reject) => {
-      login(data).then(res => {
-        context.commit('setToken', res)
-        resolve(res)
-      }).catch(error => {
-        reject(error)
-      })
-    })
+  // login(context, data) {
+  //   return new Promise((resolve, reject) => {
+  //     login(data).then(res => {
+  //       context.commit('setToken', res)
+  //       resolve(res)
+  //     }).catch(error => {
+  //       reject(error)
+  //     })
+  //   })
+  // },
+  async login({ commit }, data) {
+    const res = await login(data)
+    commit('setToken', res)
+    return res
   },
-  getUserInfo({ commit }) {
-    return new Promise((resolve, reject) => {
-      getUserInfo().then(res => {
-        commit('setUserInfo', res)
-        resolve(res)
-      }).catch(error => {
-        reject(error)
-      })
-    })
+  // getUserInfo({ commit }) {
+  //   return new Promise((resolve, reject) => {
+  //     getUserInfo().then(res => {
+  //       commit('setUserInfo', res)
+  //       resolve(res)
+  //     }).catch(error => {
+  //       reject(error)
+  //     })
+  //   })
+  // }
+  async getUserInfo({ commit }) {
+    const res = await getUserInfo()
+    const baseInfo = await getUserDetailById(res.userId)
+    const baseResult = { ...res, ...baseInfo } // 将两个接口结果合并
+    commit('setUserInfo', baseResult)
+    return baseResult
   }
 }
 
